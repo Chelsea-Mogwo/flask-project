885d12a1a2bd857a7868891ef82f1bb7c263f8b4
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-09-21 14:49:17 +0100

    add sql link to flaskenv file

--- application/__init__.py
+++ application/__init__.py
@@ -10,6 +10,6 @@ app = Flask(__name__)
 app.json_provider_class.sort_keys = False # orders the data
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://wymctuwy:zFfCnWUQJg43dofVfBVhAbAr77h3NSNk@tai.db.elephantsql.com/wymctuwy"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DB_URI"]
 
 db = SQLAlchemy(app)
